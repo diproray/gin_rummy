afc1f239fccb9cd703eb050d5591dd9e515b1175
diproray <diprorayy@gmail.com>
2018-02-20 10:25:23 -0600

    Comments, javadoc, 100-char limit/line added for StrategyTwo.java

--- src/StrategyTwo.java
+++ src/StrategyTwo.java
@@ -3,16 +3,14 @@ package com.example;
 import java.util.*;
 import java.util.stream.Collectors;
 
-/**
- * . Class modelling an average Player Strategy for Gin Rummy
- */
+/** . Class modelling an average Player Strategy for Gin Rummy */
 public class StrategyTwo implements PlayerStrategy {
 
   ArrayList<Card> hand;
 
   /**
-   * Called by the game engine for each player at the beginning of each round to receive and
-   * process their initial hand dealt.
+   * Called by the game engine for each player at the beginning of each round to receive and process
+   * their initial hand dealt.
    *
    * @param hand The initial hand dealt to the player
    */
@@ -23,18 +21,20 @@ public class StrategyTwo implements PlayerStrategy {
   }
 
   /**
-   * Called by the game engine to prompt the player on whether they want to take the top card
-   * from the discard pile or from the deck.
+   * Called by the game engine to prompt the player on whether they want to take the top card from
+   * the discard pile or from the deck.
    *
    * @param card The card on the top of the discard pile
    * @return whether the user takes the card on the discard pile
    */
   @Override
   public boolean willTakeTopDiscard(Card card) {
+
+    // Get card of largest rank.
     Card cardOfLargestRankInHand = hand.get(hand.size() - 1);
+
+    // Compare with top card of discard pile and choose what to do.
     if (card.getRank().ordinal() < cardOfLargestRankInHand.getRank().ordinal()) {
-      // hand.remove(cardOfLargestRankInHand);
-      // hand.add(card);
       return true;
     } else {
       return false;
@@ -42,29 +42,34 @@ public class StrategyTwo implements PlayerStrategy {
   }
 
   /**
-   * Called by the game engine to prompt the player to take their turn given a
-   * dealt card (and returning their card they've chosen to discard).
+   * Called by the game engine to prompt the player to take their turn given a dealt card (and
+   * returning their card they've chosen to discard).
    *
    * @param drawnCard The card the player was dealt
    * @return The card the player has chosen to discard
    */
   @Override
   public Card drawAndDiscard(Card drawnCard) {
+    // Add the drawn card to Hand.
     hand.add(drawnCard);
+
+    // Sort Hand by rank again.
     Collections.sort(hand);
+
+    // Remove card of largest rank in hand.
     Card cardBeingDiscarded = hand.get(hand.size() - 1);
     hand.remove(cardBeingDiscarded);
     return cardBeingDiscarded;
   }
 
   /**
-   * Called by the game engine to prompt the player is whether they would like to
-   * knock.
+   * Called by the game engine to prompt the player is whether they would like to knock.
    *
    * @return True if the player has decided to knock
    */
   @Override
   public boolean knock() {
+    // Knock at the first opportunity
     return true;
   }
 
@@ -72,26 +77,23 @@ public class StrategyTwo implements PlayerStrategy {
    * Called by the game engine when the opponent has finished their turn to provide the player
    * information on what the opponent just did in their turn.
    *
-   * @param drewDiscard        Whether the opponent took from the discard
+   * @param drewDiscard Whether the opponent took from the discard
    * @param previousDiscardTop What the opponent could have drawn from the discard if they chose to
-   * @param opponentDiscarded  The card that the opponent discarded
+   * @param opponentDiscarded The card that the opponent discarded
    */
   @Override
-  public void opponentEndTurnFeedback(boolean drewDiscard, Card previousDiscardTop, Card opponentDiscarded) {
-
-  }
+  public void opponentEndTurnFeedback(
+      boolean drewDiscard, Card previousDiscardTop, Card opponentDiscarded) {}
 
   /**
-   * Called by the game engine when the round has ended to provide this player strategy
-   * information about their opponent's hand and selection of Melds at the end of the round.
+   * Called by the game engine when the round has ended to provide this player strategy information
+   * about their opponent's hand and selection of Melds at the end of the round.
    *
-   * @param opponentHand  The opponent's hand at the end of the round
+   * @param opponentHand The opponent's hand at the end of the round
    * @param opponentMelds The opponent's Melds at the end of the round
    */
   @Override
-  public void opponentEndRoundFeedback(List<Card> opponentHand, List<Meld> opponentMelds) {
-
-  }
+  public void opponentEndRoundFeedback(List<Card> opponentHand, List<Meld> opponentMelds) {}
 
   /**
    * Called by the game engine to allow access the player's current list of Melds.
